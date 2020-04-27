150494b0ec19af7a65d94d3b1a953f59114a62ee
Jack Barger <jack@thetibergroup.com>
2020-04-27 10:30:14 -0600

    Fix line endings.

--- desktop/desktop.sh
+++ desktop/desktop.sh
@@ -2,7 +2,7 @@
 
 ## Packages To Customize Gnome Desktop
 sudo apt update
-sudo apt install -y /
-    gnome-tweaks /
-    gnome-shell-extensions /
+sudo apt install -y \
+    gnome-tweaks \
+    gnome-shell-extensions \
     chrome-gnome-shell
