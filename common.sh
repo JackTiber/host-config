f16fafd8c2dcb409ef04d502e1e22e1713ecb9d6
Jack Barger <jack@thetibergroup.com>
2020-04-26 19:20:46 -0600

    Update scripts with executable flag

--- common.sh
+++ common.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 ## Create Temp Folder for File(s)
-mkdir ~/temp
+mkdir -p ~/temp
 
 ## Common Packages & CLI Tools
 sudo apt update
-sudo apt install -y /
-    software-properties-common /
-    apt-transport-https /
-    curl /
-    wget /
-    jq /
-    tmux /
-    htop /
-    bash-completion /
-    ufw /
+sudo apt install -y \
+    software-properties-common \
+    apt-transport-https \
+    curl \
+    wget \
+    jq \
+    tmux \
+    htop \
+    bash-completion \
+    ufw \
     
