b1f856bc884ec5062de2a7db3bd077de680716c2
Jack Barger <jack@thetibergroup.com>
2020-06-22 18:00:15 -0600

    Switch to snap package management for ease of use.

--- desktop/desktop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## Packages To Customize Gnome Desktop
-sudo apt update
-sudo apt install -y \
-    gnome-tweaks \
-    gnome-shell-extensions \
-    chrome-gnome-shell
---
+++ desktop/extensions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+code --install-extension ms-python.python
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension ms-dotnettools.csharp
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension golang.Go
+code --install-extension christian-kohler.path-intellisense
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension formulahendry.auto-close-tag
+code --install-extension Zignd.html-css-class-completion
+code --install-extension EditorConfig.EditorConfig
+code --install-extension ms-python.anaconda-extension-pack
+code --install-extension patcx.vscode-nuget-gallery
+code --install-extension auchenberg.vscode-browser-preview
+code --install-extension wwm.better-align
+code --install-extension GitHub.github-vscode-theme
+code --install-extension vscode-icons-team.vscode-icons
