dece9073f837c2f2e4a90b1d0d98171896a8c2bc
Jack Barger <jack@thetibergroup.com>
2020-10-08 17:02:50 -0600

    Add kubectl krew plugins

---
+++ desktop/krew.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+kubectl krew install ctx
+kubectl krew install deprecations
+kubectl krew install doctor
+kubectl krew install flame
+kubectl krew install graph
+kubectl krew install images
+kubectl krew install ns
+kubectl krew install popeye
+kubectl krew install reap
+kubectl krew install tree
+kubectl krew install view-allocations
+kubectl krew install view-utilization
